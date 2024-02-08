7a54921930360d2d3966dcc998edb0e93926afe1
crowdbotics-apps <team@crowdbotics.com>
2024-02-08 14:54:50 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,10 @@
+from django.conf import settings
 from django.db import models
+
+
+class News(models.Model):
+    "Generated Model"
+    title = models.CharField(
+        max_length=255,
+    )
+    age = models.IntegerField()
